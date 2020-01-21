033d85e41f05bdd77608e22e208d4910aca0ea21
Lelio <luffery@live.fr>
2020-01-21 17:23:27 +0100

    OK
    Créez vos propres scènes par pitié

---
+++ Assets/Scripts/ProjectileTourelle.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProjectileTourelle : MonoBehaviour
+{
+    public LayerMask target;
+    
+    
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/Tourelle.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tourelle : MonoBehaviour
+{
+    public LayerMask target;
+    public float rayDistance;
+    public Transform firePoint;
+    
+    void Start()
+    {
+        Gizmos.color = Color.red;
+    }
+
+    
+    
+   
+    void Update()
+    {
+        RaycastHit[] hits = Physics.SphereCastAll(firePoint.position, rayDistance, firePoint.forward, 0f, target);
+        
+        foreach (var hit in hits)
+        {
+            Debug.Log(hit.transform.gameObject.name);
+        }
+    }
+    
+    private void OnDrawGizmos()
+    { 
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, rayDistance);
+    }
+}
